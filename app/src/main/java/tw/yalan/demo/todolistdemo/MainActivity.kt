be02d409c34a7c7c1b3b77ec25bcfb3c8def1f00
Yalan <yalandingtw@gmail.com>
2020-07-24 21:46:32 +0800

    add feature 2

--- app/src/main/java/tw/yalan/demo/todolistdemo/MainActivity.kt
+++ app/src/main/java/tw/yalan/demo/todolistdemo/MainActivity.kt
@@ -2,11 +2,13 @@ package tw.yalan.demo.todolistdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         finish()
+        findViewById<TextView>(R.id.textView)
     }
 }
